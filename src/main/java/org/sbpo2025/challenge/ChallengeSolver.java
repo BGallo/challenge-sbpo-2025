2783189e24af7af15d39e598f00272f0cf8d046d
PauloSoaresAlves <paulosoaresalves@id.uff.br>
2025-02-13 11:10:04 -0300

    finished barebones GA structure

--- src/main/java/org/sbpo2025/challenge/ChallengeSolver.java
+++ src/main/java/org/sbpo2025/challenge/ChallengeSolver.java
@@ -2,14 +2,19 @@ package org.sbpo2025.challenge;
 
 import org.apache.commons.lang3.time.StopWatch;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class ChallengeSolver {
-    private final long MAX_RUNTIME = 600000; // milliseconds; 10 minutes
+    private final long MAX_RUNTIME = 30000; // milliseconds; 30 s
 
     protected List<Map<Integer, Integer>> orders;
     protected List<Map<Integer, Integer>> aisles;
@@ -26,9 +31,191 @@ public class ChallengeSolver {
         this.waveSizeUB = waveSizeUB;
     }
 
+    public class Individual {
+        protected ArrayList<Boolean> genome;
+        protected double fitness;
+
+        public Individual(ArrayList<Boolean> genome, double fitness) {
+            this.genome = genome;
+            this.fitness = fitness;
+        }
+    }
+
     public ChallengeSolution solve(StopWatch stopWatch) {
-        // Implement your solution here
-        return null;
+        final int populationSize = 10;
+        final int maxIterations = Integer.MAX_VALUE;
+
+        //Generates the initial population
+        ArrayList<Individual> population = this.generateInitialPopulation(populationSize);
+        
+        //Evaluates the population
+        double bestScore = Integer.MIN_VALUE;
+        ChallengeSolution bestSolution = null;
+
+        for(Individual individual: population){
+            ChallengeSolution solution = this.decodeIndividual(individual);
+            double fitness = this.computeObjectiveFunction(solution);
+            individual.fitness = fitness;   
+        }
+
+        population.sort((a, b) -> Double.compare(b.fitness, a.fitness));
+
+        bestScore = population.get(0).fitness;
+        bestSolution = this.decodeIndividual(population.get(0));
+        System.out.println("Initial best score: " + bestScore);
+
+        int currentIteration = 0;
+        //Main loop
+        while(stopWatch.getTime() < MAX_RUNTIME && currentIteration < maxIterations){
+            currentIteration++;
+
+            //One point crossover
+            population = crossOverPopulation(population);
+
+            for(Individual individual: population){
+                ChallengeSolution solution = this.decodeIndividual(individual);
+                double fitness = this.computeObjectiveFunction(solution);
+                individual.fitness = fitness;   
+            }
+
+            population.sort((a, b) -> Double.compare(b.fitness, a.fitness));
+
+            //Elitist population selection
+            population.subList(populationSize, population.size()).clear();
+
+            if(population.get(0).fitness > bestScore){
+                bestScore = population.get(0).fitness;
+                bestSolution = this.decodeIndividual(population.get(0));
+            }
+        }
+
+        System.out.println("Best score: " + bestScore);
+        return bestSolution;
+    }
+
+    public ArrayList<Individual> generateInitialPopulation(int size) {
+        ArrayList<Individual> population = new ArrayList<>();
+        Random rand = new Random();
+
+        List<Integer> orderIndexes = new ArrayList<>();
+        for (int i = 0; i < orders.size(); i++) {
+            orderIndexes.add(i);
+        }
+
+
+        for (int i = 0; i < size; i++) {
+            ArrayList<Boolean> genome = new ArrayList<>(Collections.nCopies(orders.size() + aisles.size(), false));
+            int pickedUnits = 0;
+            List<Integer> localOrderIndexes = new ArrayList<>(orderIndexes);
+            List<Integer> localAisleIndexes = new ArrayList<>();
+            List<Map<Integer, Integer>> unitsTakenFromAisles = new ArrayList<>(Collections.nCopies(aisles.size(), new HashMap<>()));
+
+
+            while (localOrderIndexes.size() > 0) {
+                int orderIndex = rand.nextInt(localOrderIndexes.size());
+                int pickedOrder = localOrderIndexes.get(orderIndex);
+                int orderUnitsSum = orders.get(pickedOrder).values().stream().mapToInt(Integer::intValue).sum();
+                
+                localOrderIndexes.remove(orderIndex);
+
+                if (pickedUnits + orderUnitsSum > waveSizeUB) continue;
+
+                pickedUnits += orderUnitsSum;
+                genome.set(pickedOrder, true);
+
+                for(Map.Entry<Integer, Integer> entry: orders.get(pickedOrder).entrySet()){
+                    int item = entry.getKey();
+                    int quantity = entry.getValue();
+
+                    for(int aisleIndex = 0; aisleIndex < this.aisles.size(); aisleIndex++){
+                        Map<Integer, Integer> aisle = this.aisles.get(aisleIndex);
+                        if(aisle.containsKey(item)){
+                            int amountTakenFromItemInAisle = unitsTakenFromAisles.get(aisleIndex).getOrDefault(item, 0);
+                            int amountAvailableInAisle = aisle.get(item);
+                            if (amountTakenFromItemInAisle == amountAvailableInAisle) continue;
+                            if (!localAisleIndexes.contains(aisleIndex)) localAisleIndexes.add(aisleIndex);
+                            int newItemInAisleAmount = Math.min(amountAvailableInAisle, amountTakenFromItemInAisle + quantity);
+                            unitsTakenFromAisles.get(aisleIndex).put(item, newItemInAisleAmount);
+                            genome.set(orders.size() + aisleIndex, true);
+                            if (amountTakenFromItemInAisle + quantity < amountAvailableInAisle) break;
+                            quantity -= amountAvailableInAisle - amountTakenFromItemInAisle;
+                        }
+                    }
+
+                }      
+            }
+            population.add(new Individual(genome, -1));
+        }
+        return population;
+    }
+
+    public ChallengeSolution decodeIndividual(Individual individual) {
+        Set<Integer> selectedOrders = new HashSet<>();
+        Set<Integer> selectedAisles = new HashSet<>();
+        
+        for (int i = 0; i < orders.size(); i++) {
+            if (individual.genome.get(i)) {
+                selectedOrders.add(i);
+            }
+        }
+        
+        for (int i = 0; i < aisles.size(); i++) {
+            if (individual.genome.get(orders.size() + i)) {
+                selectedAisles.add(i);
+            }
+        }
+        
+        return new ChallengeSolution(selectedOrders, selectedAisles);
+    }
+
+    //One point crossover
+    public Individual crossover(Individual parent1, Individual parent2) {
+        Random rand = new Random();
+        int crossoverPoint = rand.nextInt(parent1.genome.size());
+        ArrayList<Boolean> childGenome = new ArrayList<>();
+        for (int i = 0; i < parent1.genome.size(); i++) {
+            if (i < crossoverPoint) {
+                childGenome.add(parent1.genome.get(i));
+            } else {
+                childGenome.add(parent2.genome.get(i));
+            }
+        }
+        Individual child =  new Individual(childGenome, -1);
+        mutate(child);
+        return child;
+    }
+
+
+    public void mutate(Individual individual) {
+        Random rand = new Random();
+        if (rand.nextDouble() > 0.1) return;
+        int mutationPoint = rand.nextInt(individual.genome.size());
+        individual.genome.set(mutationPoint, !individual.genome.get(mutationPoint));
+    }
+
+    /*
+     * Recieves an already sorted population
+     * Returns a population of size n * 1.5
+     */
+    public ArrayList<Individual> crossOverPopulation(ArrayList<Individual> population) {
+        ArrayList<Individual> newPopulation = new ArrayList<>();
+        Random rand = new Random();
+        while(population.size() > 0){
+            Individual parent1 = population.remove(0);
+            Individual parent2;
+            newPopulation.add(parent1);
+            //biased for loop
+            if (population.size() == 0) break;
+            for (int i = 0; i < population.size(); i++) {
+                if (rand.nextDouble() < 0.5 || i == population.size() - 1) {
+                    parent2 = population.remove(i);
+                    newPopulation.add(parent2);
+                    newPopulation.add(crossover(parent1, parent2));
+                }
+            }
+        }
+        return newPopulation;
+        
     }
 
     /*
@@ -86,6 +273,12 @@ public class ChallengeSolver {
         if (selectedOrders == null || visitedAisles == null || selectedOrders.isEmpty() || visitedAisles.isEmpty()) {
             return 0.0;
         }
+
+        int penalty = 0;
+        if (!isSolutionFeasible(challengeSolution)) {
+            penalty += -10;
+        }
+
         int totalUnitsPicked = 0;
 
         // Calculate total units picked
@@ -99,6 +292,6 @@ public class ChallengeSolver {
         int numVisitedAisles = visitedAisles.size();
 
         // Objective function: total units picked / number of visited aisles
-        return (double) totalUnitsPicked / numVisitedAisles;
+        return (double) (totalUnitsPicked / numVisitedAisles) + penalty;
     }
 }
